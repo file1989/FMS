960260de7ead07c2336ca7aace4d73fd2da9408a
file1989@126.com <lzp549*>
2017-01-18 21:42:34 +0800

    201701182141

---
+++ App_Code/SQLServerDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 数据访问对象
+/// </summary>
+public class SQLServerDAO
+{
+    private static string ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+	public SQLServerDAO()
+	{
+		//
+		// TODO: 在此处添加构造函数逻辑
+		//
+	}
+    
+    
+
+
+
+
+}
